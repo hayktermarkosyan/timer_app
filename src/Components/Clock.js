9f65aa70f2ab37565bf0565aea0d621f020f84f9
hayktermarkosyan <hayktermarkosyan@gmail.com>
2022-01-25 21:05:55 +0400

    first commit

---
+++ src/Components/Clock.js
@@ -0,0 +1,107 @@
+import React, {useState} from "react";
+
+let interval;
+
+const Clock = () => {
+
+  const [timerDays, setTimerDays] = useState();
+  const [timerHours, setTimerHours] = useState();
+  const [timerMinutes, setTimerMinutes] = useState();
+  const [timerSeconds, setTimerSeconds] = useState();
+
+  const eventHandler = (eventDate) => {
+    const countDownDate = new Date(eventDate).getTime();   
+          
+    if(interval){
+      clearInterval(interval);
+    }
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+
+      let distance = countDownDate - now;
+
+      if(distance < 0) {
+        distance = 0;
+      }
+
+      // days
+      distance = Math.floor(distance / 1000);
+      let s = 60 * 60 * 24;
+      let days = Math.floor(distance / s);
+
+      // hours
+      distance -= days * s;
+      s = 60 * 60;
+      let hours = Math.floor(distance / s);
+
+      // minutes
+      distance -= hours * s;
+      s = 60;
+      let minutes = Math.floor(distance / s);
+
+      // seconds
+      distance -= minutes * s;
+      let seconds = Math.floor(distance);
+
+      if (distance < 0) {
+        // Stop Timer
+        clearInterval(interval.current);
+      } else {
+        // Update Timer
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    }, 10);
+  }
+
+  if(localStorage.length === 1) {
+    eventHandler(localStorage.getItem('eventDate'));
+  }
+
+  return (
+      <div>
+        <form className="setEventDate">
+          <label htmlFor="eventDate">Date of Event:</label>
+          <input 
+            name="eventDate" 
+            id="eventDate" 
+            type="date"
+            autoComplete="off"
+            value={localStorage.getItem('eventDate')}
+            onChange={(e) => {
+              e.preventDefault();
+              localStorage.setItem('eventDate', e.target.value)
+              eventHandler(e.target.value);
+            }}
+          />
+        </form>
+        
+        <div className="clock">
+          <section>
+            <p>{timerDays}</p>
+            <small>Days</small>
+          </section>     
+          <span>:</span>
+          <section>
+            <p>{timerHours}</p>
+            <small>Hours</small>
+          </section>{" "}
+          <span>:</span>
+          <section>
+            <p>{timerMinutes}</p>
+            <small>Minutes</small>
+          </section>{" "}
+          <span>:</span>
+          <section>
+            <p>{timerSeconds}</p>
+            <small>Seconds</small>
+          </section>
+        </div>
+      </div>
+  )
+}           
+
+export default Clock;
